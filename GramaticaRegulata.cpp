d37e207e5c435822fc665921c0c7d3f8de85a659
OanaAlexandraC <36674400+OanaAlexandraC@users.noreply.github.com>
2018-04-27 09:30:22 +0300

    Add class

---
+++ GramaticaRegulata.cpp
@@ -0,0 +1,1 @@
+#include "GramaticaRegulata.h"
---
+++ GramaticaRegulata.h
@@ -0,0 +1,24 @@
+#ifndef LFA_VERIFICARE_CUVANT_GR_GRAMATICAREGULATA_H
+#define LFA_VERIFICARE_CUVANT_GR_GRAMATICAREGULATA_H
+
+class GramaticaRegulata {
+    int numarSimboluriNeterminale;
+    char *simboluriNeterminale;
+    int numarSimboluriTerminale;
+    char *simboluriTerminale;
+    int numarProductii;
+    struct productie {
+        char simbolNeterminal1, simbolTerminal, simbolNeterminal2;
+    } *reguliDeProductie;
+    char simbolCurent;
+public:
+    //explicit GramaticaRegulata(const char *fisier);
+
+    //~GramaticaRegulata();
+
+    //void afisare();
+
+    //bool verificare(char *cuvant);
+};
+
+#endif
